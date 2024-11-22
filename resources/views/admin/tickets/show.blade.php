3da74c90456eb374ab46db28b09cd7012c703a8d
SimoneCerri <simone.cerri1994@gmail.com>
2024-11-22 16:13:15 +0100

    style a bit ticket's show page

--- resources/views/admin/tickets/show.blade.php
+++ resources/views/admin/tickets/show.blade.php
@@ -1,1 +1,26 @@
-{{ $ticket->id }}
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container py-5"></div>
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <div class="card p-3">
+                <h5 class="card-title text-center">
+                    ID: {{ $ticket->id }}
+                </h5>
+                <p class="card-text">
+                    <span class="fw-bold">
+                        Description:
+                    </span>
+                    <br>
+                    {{ $ticket->description }}
+                </p>
+                <hr>
+                <button class="btn btn-dark text-danger">
+                    {{ $ticket->status }}
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
